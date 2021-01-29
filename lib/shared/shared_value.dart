a63de39549f92270354a10f5d06e2e86d3ecce86
montohang <harmonsitohang17@gmail.com>
2021-01-29 13:40:19 +0700

    [ADD Model and Theme]

---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+String apiKey = "2658e257730e43c55382cdc0fca800ef";
+String imageBaseURL = "https://image.tmdb.org/t/p/";
+
+PageEvent prevPageEvent;
+File imageFileToUpload;
